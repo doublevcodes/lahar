c034d7cfd14354464fbc57f761a68b3b2d943af2
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-05-09 22:22:34 +0100

    Rename version_string to information_string
    Follows on from commit #6e14791

--- constants.cpp
+++ constants.cpp
@@ -7,9 +7,9 @@
 std::string construct_version() {
 
     /* Lahar information */
-    std::string version_string { "Lahar v" };
-    version_string.append(LAHAR_VERSION);
-    version_string.append(" (");
+    std::string information_string { "Lahar v" };
+    information_string.append(LAHAR_VERSION);
+    information_string.append(" (");
 
     /* Date information */
     const std::chrono::time_point<std::chrono::system_clock> now{ std::chrono::system_clock::now() };
@@ -17,24 +17,24 @@ std::string construct_version() {
     std::stringstream date{};
     date << std::put_time(std::localtime(&now_time_t), "%F %T");
     std::string date_string{ date.str() };
-    version_string.append(date_string);
-    version_string.append(")");
+    information_string.append(date_string);
+    information_string.append(")");
 
     /* C++ information */
 #ifdef __cplusplus
     const std::string cpp_version{ std::to_string(__cplusplus) };
 #else
-    const std::string cpp_version{ "C++xx" };
+    const std::string cpp_version{ "######" };
 #endif
-    version_string.append(" [ C++");
-    version_string.append(cpp_version);
-    version_string.append(" ]");
+    information_string.append(" [ C++");
+    information_string.append(cpp_version);
+    information_string.append(" ]");
 
     /* Operating system information */
     std::string os = OS;
-    version_string.append(" on ");
-    version_string.append(os);
+    information_string.append(" on ");
+    information_string.append(os);
 
-    return version_string;
+    return information_string;
 };
 
